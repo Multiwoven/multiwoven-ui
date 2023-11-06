9b27affa7f900f50cf8507e13d99a1a769d83ef2
Tushar Selvakumar <tusharkumar91111@gamil.com>
2023-11-06 18:32:56 +0530

    Changed Dashboard to add new cards, added rows processed graph and recent activities of users. Changed User to John Doe

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -256,7 +256,7 @@ export const Layout =()=> {
                     />
                     <span className="hidden lg:flex lg:items-center">
                       <span className="ml-4 text-sm font-semibold leading-6 text-gray-900" aria-hidden="true">
-                        Tom Cook
+                        John Doe
                       </span>
                       <ChevronDownIcon className="ml-2 h-5 w-5 text-gray-400" aria-hidden="true" />
                     </span>
