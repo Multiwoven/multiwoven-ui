394522de1bc50f839cfbc47f9d56c38596c27aa6
amitabhRazorpay <amitabh.baruah@razorpay.com>
2024-01-02 15:59:53 +0530

    minor refactors

--- src/components/dummy.test.tsx
+++ src/components/dummy.test.tsx
@@ -1,8 +1,8 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Heading from "./Heading";
 import "@testing-library/jest-dom";
 
 test("Renders the Heading component", () => {
   render(<Heading>Something</Heading>);
-  expect(true).toBeTruthy();
+  expect(screen.getByText("Something")).toBeVisible();
 });
