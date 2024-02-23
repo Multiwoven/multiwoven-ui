6fbe0e50b68b2c9ba6084d8595597fa807a69f8c
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-02-23 18:27:31 +0530

    Changed logo to Slack, added the Slack URL and changed default URL for Docs

--- src/views/Connectors/Sources/SourcesForm/SourceFormFooter/SourceFormFooter.tsx
+++ src/views/Connectors/Sources/SourcesForm/SourceFormFooter/SourceFormFooter.tsx
@@ -1,7 +1,7 @@
 import { useUiConfig } from "@/utils/hooks";
 import { Box, Button, ButtonGroup, Icon, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import { FiBookOpen, FiHeadphones } from "react-icons/fi";
+import { FiBookOpen, FiSlack } from "react-icons/fi";
 import { Link, useNavigate } from "react-router-dom";
 
 type SourceFormFooterProps = {
@@ -31,7 +31,7 @@ const SourceFormFooter = ({
   isContinueCtaRequired = false,
   isDocumentsSectionRequired = false,
   secondaryCtaText = "Back",
-  documentationLink = "https://docs.multiwoven.com",
+  documentationLink = "https://docs.multiwoven.com/get-started/introduction",
 }: SourceFormFooterProps): JSX.Element => {
   const [leftOffset, setLeftOffet] = useState<number>(0);
   const { maxContentWidth } = useUiConfig();
@@ -80,9 +80,9 @@ const SourceFormFooter = ({
                   </Text>
                 </Box>
               </Link>
-              <Link to="https://docs.multiwoven.com">
+              <Link to="https://join.slack.com/t/multiwoven/shared_invite/zt-2bnjye26u-~lu_FFOMLpChOYxvovep7g">
                 <Box display="flex" alignItems="center">
-                  <Icon as={FiHeadphones} color="gray.600" />
+                  <Icon as={FiSlack} color="gray.600" />
                   <Text ml={2} size="sm">
                     Contact Support
                   </Text>
