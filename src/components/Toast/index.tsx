6d3514eb7d57794b61067327d9f925d815548411
Hanik Jain <hanikjain333@gmail.com>
2024-02-27 23:19:22 +0530

    CustomToast cleanup

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -16,79 +16,36 @@ const CustomToast: React.FC<ToastProps> = ({
     style={}
 }) => {
     
-    const getIcon = () => {
+    const getIcon: () => JSX.Element = () => {
         const color: string = status ? `${status}.400` : 'info.400';
-        
+
         if (status === 'success') {
           return <Icon as={CheckCircleIcon} color={color} marginRight="16px"  boxSize="20px" />;
         } else {
           return <Icon as={InfoIcon} color={color} marginRight="16px" boxSize="20px" />;
         }
-        // Add more conditions for other statuss if needed
-      };
+        
+    };
 
-    if(status === 'success') return (
-        <Box 
-            bg="success.100" 
-            border="1px solid" 
-            borderColor="success.200" 
-            width="400px"
-            paddingX="16px" 
-            paddingY="20px" 
-            borderRadius="8px" 
-            >
-                {getIcon()}
-                {title}
-        </Box>
-    );  
+    if(status) {
+        const bg : string = `${status}.100`;
+        const borderColor: string = `${status}.200`;
+        return (
+            <Box 
+                bg={bg} 
+                border="1px solid" 
+                borderColor={borderColor} 
+                width="400px"
+                paddingX="16px" 
+                paddingY="25px" 
+                borderRadius="8px" 
+                >
+                    {getIcon()}
+                    {title}
+            </Box>
+        );  
+    }
 
-    if(status === 'info') return (
-        <Box 
-            bg="info.100" 
-            border="1px solid" 
-            borderColor="info.200" 
-            width="400px"
-            paddingX="16px" 
-            paddingY="20px" 
-            borderRadius="8px" 
-            >
-                {getIcon()}
-                {title}
-        </Box>
-    ); 
-
-
-    if(status === 'warning') return (
-        <Box 
-            bg="warning.100" 
-            border="1px solid" 
-            borderColor="warning.200" 
-            width="400px"
-            paddingX="16px" 
-            paddingY="20px" 
-            borderRadius="8px" 
-            >
-                {getIcon()}
-                {title}
-        </Box>
-    ); 
-
-
-    if(status === 'error') return (
-        <Box 
-            bg="error.100" 
-            border="1px solid" 
-            borderColor="error.200" 
-            width="400px"
-            paddingX="16px" 
-            paddingY="20px" 
-            borderRadius="8px" 
-            >   
-                {getIcon()}
-                {title}
-        </Box>
-    ); 
-    
     <Box style={style}> {getIcon()} {title}</Box>
 }
 
