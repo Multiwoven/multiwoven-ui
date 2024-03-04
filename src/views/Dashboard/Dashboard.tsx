59e996ac9a0f4552139b67d6b8f1e5a36c3a2e1e
sumitd94 <sumitdhanania94@gmail.com>
2024-03-05 01:11:14 +0530

    feat: list all connectors

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -1,6 +1,10 @@
-import { Stack, Tab, TabIndicator, TabList, Tabs, Box, Text } from '@chakra-ui/react';
+import { Stack, Tab, TabIndicator, TabList, Tabs, Box, Text, Checkbox } from '@chakra-ui/react';
 import ContentContainer from '@/components/ContentContainer';
 import TopBar from '@/components/TopBar';
+import { useQuery } from '@tanstack/react-query';
+
+import { getAllConnectors } from '@/services/connectors';
+import EntityItem from '@/components/EntityItem';
 
 const TabName = ({ title }: { title: string }) => (
   <Tab
@@ -18,6 +22,16 @@ const TabName = ({ title }: { title: string }) => (
 );
 
 const Dashboard = (): JSX.Element => {
+  const { data, isLoading } = useQuery({
+    queryKey: ['models'],
+    queryFn: () => getAllConnectors(),
+    refetchOnMount: true,
+    refetchOnWindowFocus: false,
+  });
+  console.log(data);
+
+  const connectorsList = data?.data;
+
   return (
     <Box width='100%' display='flex' flexDirection='column' alignItems='center'>
       <ContentContainer>
@@ -29,25 +43,48 @@ const Dashboard = (): JSX.Element => {
           isCtaVisible={false}
         />
         <Box>
-          <Stack spacing='16'>
-            <Tabs
-              size='md'
-              variant='indicator'
-              background='gray.300'
-              padding={1}
+          <Stack gap='24px'>
+            <Stack spacing='16'>
+              <Tabs
+                size='md'
+                variant='indicator'
+                background='gray.300'
+                padding={1}
+                borderRadius='8px'
+                borderStyle='solid'
+                borderWidth='1px'
+                borderColor='gray.400'
+                width='352px'
+              >
+                <TabList gap='8px'>
+                  <TabName title='All Connectors' />
+                  <TabName title='By Destination' />
+                  <TabName title='By Source' />
+                </TabList>
+                <TabIndicator />
+              </Tabs>
+            </Stack>
+            <Box
+              height='460px'
+              backgroundColor='gray.100'
+              width='352px'
               borderRadius='8px'
               borderStyle='solid'
               borderWidth='1px'
               borderColor='gray.400'
-              width='352px'
             >
-              <TabList>
-                <TabName title='All Connectors' />
-                <TabName title='By Destination' />
-                <TabName title='By Source' />
-              </TabList>
-              <TabIndicator />
-            </Tabs>
+              <Stack gap='12px'>
+                {connectorsList?.map((connector, index) => (
+                  <Box key={index} paddingY='10px' paddingX='16px' display='flex' gap='12px'>
+                    <Checkbox size='md' colorScheme='#F54C3D' />
+                    <EntityItem
+                      icon={connector?.attributes?.icon}
+                      name={connector?.attributes?.name}
+                    />
+                  </Box>
+                ))}
+              </Stack>
+            </Box>
           </Stack>
         </Box>
       </ContentContainer>
