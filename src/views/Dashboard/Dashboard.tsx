a4fe0c3d5eabc009935310ed05fc047c57715773
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-03-05 02:08:32 +0530

    Fixed spacing errors

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -92,62 +92,60 @@ const Dashboard = (): JSX.Element => {
         />
 
         <Box display={{ base: 'flex flex-col', lg: 'flex' }} gap='24px'>
-          <Stack gap='24px'>
-            <Stack spacing='16'>
-              <Tabs
-                size='md'
-                variant='indicator'
-                background='gray.300'
-                padding={1}
-                borderRadius='8px'
-                borderStyle='solid'
-                borderWidth='1px'
-                borderColor='gray.400'
-                width='352px'
-              >
-                <TabList gap='8px'>
-                  <TabName title='All Connectors' />
-                  <TabName title='By Destination' />
-                  <TabName title='By Source' />
-                </TabList>
-                <TabIndicator />
-              </Tabs>
-              <Box
-                height='460px'
-                backgroundColor='gray.100'
-                width='352px'
-                borderRadius='8px'
-                borderStyle='solid'
-                borderWidth='1px'
-                borderColor='gray.400'
-              >
-                <Stack gap='12px' height='100%'>
-                  {connectorsList?.length === 0 && (
-                    <VStack justify='center' height='100%'>
-                      <Text color='gray.600' size='xs' fontWeight='semibold'>
-                        No connectors found
-                      </Text>
-                    </VStack>
-                  )}
-                  {connectorsList?.map((connector, index) => (
-                    <Box
-                      key={index}
-                      paddingY='10px'
-                      paddingX='16px'
-                      display='flex'
-                      gap='12px'
-                      _hover={{ backgroundColor: 'gray.200' }}
-                    >
-                      <Checkbox size='md' />
-                      <EntityItem
-                        icon={connector?.attributes?.icon}
-                        name={connector?.attributes?.name}
-                      />
-                    </Box>
-                  ))}
-                </Stack>
-              </Box>
-            </Stack>
+          <Stack gap='32px'>
+            <Tabs
+              size='md'
+              variant='indicator'
+              background='gray.300'
+              padding={1}
+              borderRadius='8px'
+              borderStyle='solid'
+              borderWidth='1px'
+              borderColor='gray.400'
+              width='352px'
+            >
+              <TabList gap='8px'>
+                <TabName title='All Connectors' />
+                <TabName title='By Destination' />
+                <TabName title='By Source' />
+              </TabList>
+              <TabIndicator />
+            </Tabs>
+            <Box
+              height='460px'
+              backgroundColor='gray.100'
+              width='352px'
+              borderRadius='8px'
+              borderStyle='solid'
+              borderWidth='1px'
+              borderColor='gray.400'
+            >
+              <Stack gap='12px' height='100%'>
+                {connectorsList?.length === 0 && (
+                  <VStack justify='center' height='100%'>
+                    <Text color='gray.600' size='xs' fontWeight='semibold'>
+                      No connectors found
+                    </Text>
+                  </VStack>
+                )}
+                {connectorsList?.map((connector, index) => (
+                  <Box
+                    key={index}
+                    paddingY='10px'
+                    paddingX='16px'
+                    display='flex'
+                    gap='12px'
+                    _hover={{ backgroundColor: 'gray.200' }}
+                  >
+                    <Checkbox size='md' />
+                    <EntityItem
+                      icon={connector?.attributes?.icon}
+                      name={connector?.attributes?.name}
+                    />
+                  </Box>
+                ))}
+              </Stack>
+            </Box>
           </Stack>
           <Stack gap='32px'>
             <Stack spacing='16'>
