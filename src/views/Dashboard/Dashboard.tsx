0ffeeef24f5ec8ef0b73fc43c56c3ecf81128723
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-03-05 00:09:38 +0530

    Added all cards to the dashboard

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Stack, Tab, TabIndicator, TabList, Tabs, Box, Text } from '@chakra-ui/react';
+import { Stack, Tab, TabIndicator, TabList, Tabs, Box, Text, VStack } from '@chakra-ui/react';
 import ContentContainer from '@/components/ContentContainer';
 import TopBar from '@/components/TopBar';
 import {
@@ -7,46 +7,18 @@ import {
   LinearScale,
   BarElement,
   Title,
-  Tooltip,
   Legend,
+  Tooltip,
 } from 'chart.js';
-import { Bar } from 'react-chartjs-2';
+import { useQuery } from '@tanstack/react-query';
+import { getReport } from '@/services/dashboard';
+import SyncRuns from './SyncRuns';
+import SyncRunsErrors from './SyncRunsErrors';
+import RowsProcessed from './RowsProcessed';
+import RowsFailed from './RowsFailed';
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
-const data = {
-  labels: ['2024-03-04T10:10:00.000Z'],
-  datasets: [
-    {
-      label: 'Total',
-      data: [2],
-      backgroundColor: 'skyblue',
-    },
-    {
-      label: 'Failed',
-      data: [0],
-      backgroundColor: 'lightcoral',
-    },
-    {
-      label: 'Success',
-      data: [2],
-      backgroundColor: 'lightgreen',
-    },
-  ],
-};
-
-const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top' as const,
-    },
-    title: {
-      display: true,
-      text: 'Chart.js Bar Chart',
-    },
-  },
-};
 const TabName = ({ title }: { title: string }) => (
   <Tab
     _selected={{
@@ -63,6 +35,24 @@ const TabName = ({ title }: { title: string }) => (
 );
 
 const Dashboard = (): JSX.Element => {
+  const { data, isLoading } = useQuery({
+    queryKey: ['dashboard', 'syncs'],
+    queryFn: () => getReport(),
+    refetchOnMount: true,
+    refetchOnWindowFocus: false,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (!data) {
+    return <div>Error</div>;
+  }
+
+  console.log(data.data.sync_run_triggered);
+  const syncRunTriggeredData = data.data.sync_run_triggered;
+  const syncRunRowsData = data.data.total_sync_run_rows;
+
   return (
     <Box width='100%' display='flex' flexDirection='column' alignItems='center'>
       <ContentContainer>
@@ -73,29 +63,61 @@ const Dashboard = (): JSX.Element => {
           onCtaClicked={() => {}}
           isCtaVisible={false}
         />
-        <Box>
-          <Stack spacing='16'>
-            <Tabs
-              size='md'
-              variant='indicator'
-              background='gray.300'
-              padding={1}
-              borderRadius='8px'
-              borderStyle='solid'
-              borderWidth='1px'
-              borderColor='gray.400'
-              width='352px'
-            >
-              <TabList>
-                <TabName title='All Connectors' />
-                <TabName title='By Destination' />
-                <TabName title='By Source' />
-              </TabList>
-              <TabIndicator />
-            </Tabs>
+        <Box display={{ base: 'flex flex-col', lg: 'flex' }} gap='24px'>
+          <Box>
+            <Stack spacing='16'>
+              <Tabs
+                size='md'
+                variant='indicator'
+                background='gray.300'
+                padding={1}
+                borderRadius='8px'
+                borderStyle='solid'
+                borderWidth='1px'
+                borderColor='gray.400'
+                width='352px'
+              >
+                <TabList>
+                  <TabName title='All Connectors' />
+                  <TabName title='By Destination' />
+                  <TabName title='By Source' />
+                </TabList>
+                <TabIndicator />
+              </Tabs>
+            </Stack>
+          </Box>
+          <Stack gap='32px'>
+            <Stack spacing='16'>
+              <Tabs
+                size='md'
+                variant='indicator'
+                background='gray.300'
+                padding={1}
+                borderRadius='8px'
+                borderStyle='solid'
+                borderWidth='1px'
+                borderColor='gray.400'
+                width='fit-content'
+              >
+                <TabList>
+                  <TabName title='24h' />
+                  <TabName title='7d' />
+                </TabList>
+                <TabIndicator />
+              </Tabs>
+            </Stack>
+            <Box display={{ base: 'flex flex-col', lg: 'flex' }} gap={3}>
+              <VStack gap={3}>
+                <SyncRuns syncRunTriggeredData={syncRunTriggeredData} />
+                <SyncRunsErrors syncRunTriggeredData={syncRunTriggeredData} />
+              </VStack>
+              <VStack gap={3}>
+                <RowsProcessed rowsProcessedData={syncRunRowsData} />
+                <RowsFailed rowsFailedData={syncRunRowsData} />
+              </VStack>
+            </Box>
           </Stack>
         </Box>
-        <Bar options={options} data={data} />
       </ContentContainer>
     </Box>
   );
