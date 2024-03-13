6c2134c956fdbeca753c35f05c3c25ea1a093c86
sumitd94 <sumitdhanania94@gmail.com>
2024-03-13 14:54:28 +0530

    refactor: move template options in a different folder

--- src/views/Activate/Syncs/SyncForm/ConfigureSyncs/TemplateOptions/TemplateOptions.tsx
+++ src/views/Activate/Syncs/SyncForm/ConfigureSyncs/TemplateOptions/TemplateOptions.tsx
@@ -10,13 +10,10 @@ import {
   PopoverTrigger,
   PopoverContent,
   Input,
-  InputGroup,
-  InputLeftElement,
-  Icon,
 } from '@chakra-ui/react';
-import { FiSearch } from 'react-icons/fi';
+import Columns from './Columns';
 
-type TemplateDropdownProps = {
+type TemplateOptionsProps = {
   entityName: string;
   isDisabled: boolean;
   columnOptions: string[];
@@ -39,11 +36,11 @@ const TabName = ({ title, filterConnectors }: { title: string; filterConnectors:
   </Tab>
 );
 
-const TemplateDropdown = ({
+const TemplateOptions = ({
   entityName,
   isDisabled,
   columnOptions,
-}: TemplateDropdownProps): JSX.Element => {
+}: TemplateOptionsProps): JSX.Element => {
   return (
     <Popover placement='bottom-start'>
       <PopoverTrigger>
@@ -94,36 +91,7 @@ const TemplateDropdown = ({
               </Tabs>
             </Stack>
             <Box backgroundColor='gray.100'>
-              <Stack gap='12px' height='100%'>
-                <InputGroup>
-                  <InputLeftElement pointerEvents='none'>
-                    <Icon as={FiSearch} color='gray.600' boxSize='5' />
-                  </InputLeftElement>
-                  <Input
-                    placeholder='Search Columns'
-                    _placeholder={{ color: 'gray.600' }}
-                    borderColor='black.500'
-                    _hover={{ borderColor: 'black.500' }}
-                  />
-                </InputGroup>
-                <Box height='180px' overflowY='auto'>
-                  {columnOptions.map((column, index) => (
-                    <Box
-                      key={index}
-                      paddingY='10px'
-                      paddingX='16px'
-                      display='flex'
-                      gap='12px'
-                      _hover={{
-                        backgroundColor: 'gray.200',
-                      }}
-                      cursor='pointer'
-                    >
-                      <Text size='sm'>{column}</Text>
-                    </Box>
-                  ))}
-                </Box>
-              </Stack>
+              <Columns columnOptions={columnOptions} />
             </Box>
           </Stack>
         </Box>
@@ -132,4 +100,4 @@ const TemplateDropdown = ({
   );
 };
 
-export default TemplateDropdown;
+export default TemplateOptions;
