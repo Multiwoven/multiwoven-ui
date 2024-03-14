1f1943bc6d4f44e51521853ac4098754727342c6
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-03-15 03:29:04 +0530

    feat: added variants to status tag

--- src/components/StatusTag/StatusTag.tsx
+++ src/components/StatusTag/StatusTag.tsx
@@ -1,22 +1,52 @@
 import { Tag, Text } from '@chakra-ui/react';
 
-const StatusTag = ({ status }: { status: string }) => (
-  <Tag
-    colorScheme='teal'
-    size='xs'
-    bgColor='success.100'
-    paddingX={2}
-    fontWeight={600}
-    borderColor='success.300'
-    borderWidth='1px'
-    borderStyle='solid'
-    height='22px'
-    borderRadius='4px'
-  >
-    <Text size='xs' fontWeight='semibold' color='success.600'>
-      {status}
-    </Text>
-  </Tag>
-);
+type StatusTagVariants = 'success' | 'error';
+
+type StatusTagProps = {
+  variant?: StatusTagVariants;
+  status: string;
+};
+
+type VariantTheme = Record<
+  StatusTagVariants,
+  {
+    bgColor: string;
+    borderColor: string;
+    textColor: string;
+  }
+>;
+
+const StatusTag = ({ status, variant = 'success' }: StatusTagProps) => {
+  const theme: VariantTheme = {
+    success: {
+      bgColor: 'success.100',
+      borderColor: 'success.300',
+      textColor: 'success.600',
+    },
+    error: {
+      bgColor: 'error.100',
+      borderColor: 'error.300',
+      textColor: 'error.600',
+    },
+  };
+  return (
+    <Tag
+      colorScheme='teal'
+      size='xs'
+      bgColor={theme[variant].bgColor}
+      paddingX={2}
+      fontWeight={600}
+      borderColor={theme[variant].borderColor}
+      borderWidth='1px'
+      borderStyle='solid'
+      height='22px'
+      borderRadius='4px'
+    >
+      <Text size='xs' fontWeight='semibold' color={theme[variant].textColor}>
+        {status}
+      </Text>
+    </Tag>
+  );
+};
 
 export default StatusTag;
